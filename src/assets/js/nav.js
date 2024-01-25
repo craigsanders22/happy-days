9565ed2ed3991769d1036018813f1847c9775a0d
Craig Sanders <126110836+craigsanders22@users.noreply.github.com>
2024-01-24 19:43:56 -0500

    fix deploy

--- src/assets/js/nav.js
+++ src/assets/js/nav.js
@@ -26,11 +26,12 @@ function ariaExpanded() {
 	}
 }
 
+
 	// This script adds a class to the body after scrolling 100px
 // and we used these body.scroll styles to create some on scroll 
 // animations with the navbar
 
-document.addEventListener('scroll', (e) => { 
+document.addEventListener('scroll', () => { 
 	const scroll = document.documentElement.scrollTop;
 	if(scroll >= 100){
 document.querySelector('body').classList.add('scroll')
